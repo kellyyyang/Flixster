c3e04480fb702fb9442c0b754ff22dcefba50bdc
kellyyyang <78131256+kellyyyang@users.noreply.github.com>
2022-06-03 10:12:50 -0700

    Added a placeholder image for Poster Path.

--- app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
@@ -68,7 +68,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            // TODO: attempting to have placeholder image
+            Glide.with(context)
+                    .load(movie.getPosterPath())
+                    .placeholder(R.drawable.flicks_movie_placeholder).
+                    into(ivPoster);
         }
     }
 }
