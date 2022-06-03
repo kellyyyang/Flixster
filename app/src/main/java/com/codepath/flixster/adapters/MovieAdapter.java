6c794ae4dbb91f1c2f9f00d863efacd30791bd46
kellyyyang <78131256+kellyyyang@users.noreply.github.com>
2022-06-03 14:31:47 -0700

    Rounded image corners.

--- app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
@@ -6,6 +6,7 @@ import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.RoundedCorner;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -15,6 +16,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.codepath.flixster.MovieDetailsActivity;
 import com.codepath.flixster.R;
 import com.codepath.flixster.models.Movie;
@@ -88,10 +92,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
                 imagePlaceholder = R.drawable.flicks_movie_placeholder;
             }
 
+            // rounded corners
+            int radius = 30;
+
             Glide.with(context)
                     .load(imageUrl)
-                    .placeholder(imagePlaceholder).
-                    into(ivPoster);
+                    .transform(new RoundedCorners(radius))
+                    .placeholder(imagePlaceholder)
+                    .into(ivPoster);
         }
 
         @Override
